df65003ecb68085f2ffeda43944fa4858549aea3
Alexandra <alexandra.imereli@gmail.com>
2016-07-01 14:09:55 +0300

    Added Comic Books Detail view

--- src/ComicBookGalery/Controllers/ComicBooksController.cs
+++ src/ComicBookGalery/Controllers/ComicBooksController.cs
@@ -10,13 +10,7 @@ namespace ComicBookGalery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic book controller");
-           
+            return View();
         }
     }
 }
