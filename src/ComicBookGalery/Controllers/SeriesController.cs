b86754948b7a56998cf180e111b8c364a16f855d
Alexandra <alexandra.imereli@gmail.com>
2016-07-09 22:35:00 +0300

    Added Series List view

---
+++ src/ComicBookGalery/Controllers/SeriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ComicBookGalery.Data;
+
+namespace ComicBookGalery.Controllers
+{
+    public class SeriesController : Controller
+    {
+        private SeriesRepository _seriesRepository = null;
+        public SeriesController()
+        {
+            _seriesRepository = new SeriesRepository();
+        }
+        public ActionResult Index()
+        {
+            var series = _seriesRepository.GetSeries();
+            return View(series);
+        }
+    }
+}
